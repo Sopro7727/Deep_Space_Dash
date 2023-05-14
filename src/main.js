a9b57f791f0aebef3aaa7be5bcef15d6405465b5
Noah Walker <nwalker7727@gmail.com>
2023-05-14 10:07:40 -0700

    Entered BGround Image and part coll detect

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 /* 
     Noah Walker
     Endless Runner Name:
-    Hours Spent(ish): 
+    Hours Spent(ish): 8
 */
 let config = {
     type: Phaser.CANVAS,
     width: 800,
     height: 800,
+    physics:{
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    backgroundColor: 'white',
     scene: [Start, Play, End]
 }
 let game = new Phaser.Game(config);
@@ -14,3 +21,4 @@ let borderUISize = game.config.height/2;
 let borderPadding = borderUISize/2;
 let score = 0;
 let keyUP, keyDOWN, keyENTER;
+let centerX = game.config.width/2;
